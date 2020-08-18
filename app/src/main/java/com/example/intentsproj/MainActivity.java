aa0129463045608443f57194c885514b8df7b263
MahendraThammita <dhmmpthammita@gmail.com>
2020-08-18 23:37:04 +0530

    compleated

--- app/src/main/java/com/example/intentsproj/MainActivity.java
+++ app/src/main/java/com/example/intentsproj/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -25,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
         inNum1 = findViewById(R.id.actOneNum1);
         inNum2 = findViewById(R.id.actOneNum2);
 
+        LayoutInflater layin = getLayoutInflater();
+        View layout = layin.inflate(R.layout.custom_toast , (ViewGroup) findViewById(R.id.custom_toast_layout));
+
+        Toast editToast =new Toast(getApplicationContext());
+        editToast.setDuration(Toast.LENGTH_SHORT);
+        editToast.setGravity(Gravity.CENTER_VERTICAL , 0 , 0);
+        editToast.setView(layout);
+        editToast.show();
 
         final Context cont = getApplicationContext();
         final Intent toNext = new Intent(cont , SecondActivity.class);
--- app/src/main/java/com/example/intentsproj/SecondActivity.java
+++ app/src/main/java/com/example/intentsproj/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,8 +28,43 @@ public class SecondActivity extends AppCompatActivity {
         no2Show = findViewById(R.id.no2DispArea);
         showCalc = findViewById(R.id.showCalc);
 
+
+
         Intent recieverIntent = getIntent();
         no1Show.setText(String.valueOf(recieverIntent.getIntExtra("VAL_NO1" , 0)));
         no2Show.setText(String.valueOf(recieverIntent.getIntExtra("VAL_NO2" , 0)));
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int ans;
+                ans = Integer.parseInt(no1Show.getText().toString()) + Integer.parseInt(no2Show.getText().toString());
+                showCalc.setText(no1Show.getText() + " + " + no2Show.getText() +" = " + ans);
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int ans;
+                ans = Integer.parseInt(no1Show.getText().toString()) - Integer.parseInt(no2Show.getText().toString());
+                showCalc.setText(no1Show.getText() + " - " + no2Show.getText() +" = " + ans);
+            }
+        });
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long ans;
+                ans = Integer.parseInt(no1Show.getText().toString()) / Integer.parseInt(no2Show.getText().toString());
+                showCalc.setText(no1Show.getText() + " / " + no2Show.getText() +" = " + ans);
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int ans;
+                ans = Integer.parseInt(no1Show.getText().toString()) * Integer.parseInt(no2Show.getText().toString());
+                showCalc.setText(no1Show.getText() + " * " + no2Show.getText() +" = " + ans);
+            }
+        });
     }
 }
